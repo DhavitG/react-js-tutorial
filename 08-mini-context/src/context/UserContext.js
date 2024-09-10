98ede6c334f469f94e57daf6878104e18f5daf9d
DhavitG <gdhavit@gmail.com>
2024-09-11 02:31:39 +0530

    context-api-basics

---
+++ 08-mini-context/src/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UserContext = React.createContext(); // global variable that stores data, which can be accessed from anywhere at all times
+
+export default UserContext;
